0fe5cc1cb251c226b21333cf05af4b3e2445f661
Aman Gupta <techbugaman@gmail.com>
2022-09-02 23:09:28 +0530

    Added logic for adding user data to database but didn't add custom error message. Install express validator package for that

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+// MiddleWare (if you want to use req.body)
+app.use(express.json()); 
+
 // Available Routes
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express= require('express');
+const User = require('../models/User');
 const router=express.Router();//express ke andar router hota hai
 
-router.get('/',(req,res)=>{
-    res.json([]);//passing a null object
+// Create a User using : POST "/api/auth/". No login
+// Use POST  not GET bcoz GET se URL me information chipakar aati hai and then your password won't be safe
+router.post('/',(req,res)=>{
+    console.log(req.body);
+    // Making a user 
+    const user= User(req.body);
+    user.save(); // Data now stored in the database (Open your MongoDb Compass to check)
+    res.send(req.body);
 })
 
 module.exports= router;
